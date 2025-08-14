23030c23195cd1c7263271745d4e530fa99d337a
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-15 00:23:15 +0200

    Invoice Status

---
+++ src/MechanicDomain/WorkOrders/Billing/InvoiceStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MechanicDomain.WorkOrders.Billing
+{
+    public enum InvoiceStatus
+    {
+        Unpaid = 0,
+        Paid = 1,
+        Refunded = 2
+    }
+}

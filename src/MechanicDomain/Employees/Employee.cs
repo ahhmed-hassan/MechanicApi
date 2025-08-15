2042e4fbb9b8147dc4f8d71b9e1225eeafcd3b6c
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-15 15:58:03 +0200

    Employee

---
+++ src/MechanicDomain/Employees/Employee.cs
@@ -0,0 +1,45 @@
+﻿using ErrorOr;
+using MechanicDomain.Abstractions;
+namespace MechanicDomain.Employees;
+
+public sealed class Employee : AuditableEntity
+{
+    public string FirstName { get; }
+    public string LastName { get; }
+    public Role Role { get; }
+    public string FullName => $"{FirstName} {LastName}";
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+    private Employee()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+    { }
+
+    private Employee(Guid id, string firstName, string lastName, Role role)
+        : base(id)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Role = role;
+    }
+
+    public static ErrorOr<Employee> Create(Guid id, string firstName, string lastName, Role role)
+    {
+        if (id == Guid.Empty)   
+            return EmployeeErrors.IdRequired;
+        
+
+        if (string.IsNullOrWhiteSpace(firstName))       
+            return EmployeeErrors.FirstNameRequired;
+        
+        if (string.IsNullOrWhiteSpace(lastName))        
+            return EmployeeErrors.LastNameRequired;
+        
+
+        if (!Enum.IsDefined(role))        
+            return EmployeeErrors.RoleInvalid;
+        
+
+        return new Employee(id, firstName.Trim(), lastName.Trim(), role);
+    }
+}

ab833032d74c91bb16d78a8904c086ef7ccee81b
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-16 19:25:22 +0200

    Handler

--- src/MechanicApplication/Features/Customers/Commands/UpdateCustomer/UpdateVehicleCommand.cs
+++ src/MechanicApplication/Features/Customers/Commands/UpdateCustomer/UpdateVehicleCommand.cs
@@ -4,7 +4,7 @@ using MediatR;
 namespace MechanicApplication.Features.Customers.Commands.UpdateCustomer;
 
 public sealed record UpdateVehicleCommand(
- Guid? VehicleId,
+ Guid VehicleId,
  string Make,
  string Model,
  int Year,

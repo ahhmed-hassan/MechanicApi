5d46c5835e6e2c17445847927857a864b0bbabfa
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-16 18:56:14 +0200

    Commands

---
+++ src/MechanicApplication/Features/Customers/Commands/UpdateCustomerCommand.cs
@@ -0,0 +1,15 @@
+﻿using ErrorOr;
+using MediatR;
+using System.Collections.Immutable;
+
+
+namespace MechanicApplication.Features.Customers.Commands;
+
+public sealed record UpdateCustomerCommand(
+ Guid CustomerId,
+ string Name,
+ string PhoneNumber,
+ string Email,
+ ImmutableList<UpdateVehicleCommand> Vehicles
+
+) : IRequest<ErrorOr<Updated>>;
---
+++ src/MechanicApplication/Features/Customers/Commands/UpdateVehicleCommand.cs
@@ -0,0 +1,11 @@
+﻿using ErrorOr;
+using MediatR;
+
+namespace MechanicApplication.Features.Customers.Commands;
+
+public sealed record UpdateVehicleCommand(
+ Guid? VehicleId,
+ string Make,
+ string Model,
+ int Year,
+ string LicensePlate) : IRequest<ErrorOr<Updated>>;

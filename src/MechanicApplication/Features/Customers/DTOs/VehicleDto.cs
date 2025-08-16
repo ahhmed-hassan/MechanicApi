2b08e101288174092d027e8855fb3feff16de059
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-16 14:37:21 +0200

    Vehicle and Customer DTO

---
+++ src/MechanicApplication/Features/Customers/DTOs/VehicleDto.cs
@@ -0,0 +1,13 @@
+﻿
+using MechanicDomain.Customers;
+
+namespace MechanicApplication.Features.Customers.DTOs;
+
+public record VehicleDto(Guid VehicleId, string Make, string Model, int Year, string LicensePlate)
+{
+    public virtual bool Equals(VehicleDto? other)
+       => other is not null && VehicleId == other.VehicleId;
+
+    public override int GetHashCode()
+        => VehicleId.GetHashCode();
+};

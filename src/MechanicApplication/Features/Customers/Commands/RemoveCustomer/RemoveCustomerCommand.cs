fb8fdd3644e1a01c16a0d830b253eb33fba94531
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-16 19:29:46 +0200

    Remove Command

---
+++ src/MechanicApplication/Features/Customers/Commands/RemoveCustomer/RemoveCustomerCommand.cs
@@ -0,0 +1,6 @@
+﻿using ErrorOr;
+using MediatR;
+
+namespace MechanicApplication.Features.Customers.Commands.RemoveCustomer;
+public sealed record RemoveCustomerCommand(Guid CustomerId)
+    : IRequest<ErrorOr<Deleted>>;

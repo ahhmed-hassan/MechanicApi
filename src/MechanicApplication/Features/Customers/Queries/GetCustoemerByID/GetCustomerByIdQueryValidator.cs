b6e2e8226e57e7f0331fff21cc9517d3cf08d010
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-17 11:11:27 +0200

    GetCustoemrByIdQuery

---
+++ src/MechanicApplication/Features/Customers/Queries/GetCustoemerByID/GetCustomerByIdQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace MechanicApplication.Features.Customers.Queries.GetCustoemerByID;
+
+public sealed class GetCustomerByIdQueryValidator : AbstractValidator<GetCustomerByIdQuery>
+{
+    public GetCustomerByIdQueryValidator()
+    {
+        RuleFor(request => request.CustomerId)
+            .NotEmpty()
+            .WithErrorCode("CustomerId_Is_Required")
+            .WithMessage("CustomerId is required.");
+    }
+}

a7e9eaba421b6c1e267e7e4cb0be8d37bcaab90f
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-17 11:31:11 +0200

    Iuser, IIdentityService, IInvoicePdfGenerator interfaces

---
+++ src/MechanicApplication/Common/Interfaces/IIdenttiyService.cs
@@ -0,0 +1,21 @@
+﻿using ErrorOr;
+
+namespace MechanicApplication.Common.Interfaces;
+
+public interface IIdenttiyService
+{
+    Task<bool> IsInRoleAsync(string userId, string role);
+
+    Task<bool> AuthorizeAsync(string userId, string? policyName);
+
+    Task<ErrorOr<AppUserDto>> AuthenticateAsync(string email, string password);
+
+    Task<ErrorOr<AppUserDto>> GetUserByIdAsync(string userId);
+
+    Task<string?> GetUserNameAsync(string userId);
+}
+
+//TODO: Deelte this soon
+public class AppUserDto
+{
+}

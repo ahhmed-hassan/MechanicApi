02084bc725ec09cecba44c0a28a960f7b5ddb376
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-17 10:22:53 +0200

    CachingBehaviour and cache Query Interface

---
+++ src/MechanicApplication/Common/Behaviors/CachingBehavior.cs
@@ -0,0 +1,57 @@
+﻿
+using ErrorOr;
+using MechanicApplication.Common.Interfaces;
+using MediatR;
+using Microsoft.Extensions.Caching.Hybrid;
+using Microsoft.Extensions.Logging;
+
+namespace MechanicApplication.Common.Behaviors;
+
+public class CachingBehavior<TRequest, TResponse>(
+    ILogger<CachingBehavior<TRequest, TResponse>> logger,
+    HybridCache cache)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    where TResponse : notnull
+{
+    private readonly HybridCache _cache = cache;
+    private readonly ILogger<CachingBehavior<TRequest, TResponse>> _logger = logger;
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        // Pass all non-cache requests through
+        if (request is not ICachedQuery cachedQuery)
+        {
+            return await next();
+        }
+        _logger.LogInformation("Checking cache for {RequestName}", typeof(TRequest).Name);
+
+        var result = await _cache.GetOrCreateAsync<TResponse>(
+           key: cachedQuery.CacheKey,
+            factory: _ => new ValueTask<TResponse>((TResponse)(object)(null!)),
+            options: new HybridCacheEntryOptions { Flags = HybridCacheEntryFlags.DisableUnderlyingData },
+            cancellationToken: cancellationToken
+           );
+
+        if (result is null)
+        {
+            result = await next(cancellationToken);
+            if (result is IErrorOr {IsError : false})
+            {
+                _logger.LogInformation("Caching result for {RequestName} with key {CacheKey}", typeof(TRequest).Name, cachedQuery.CacheKey);
+                await _cache.SetAsync(
+                     cachedQuery.CacheKey,
+                     result,
+                      new HybridCacheEntryOptions { Expiration = cachedQuery.Expiration },
+                     cachedQuery.Tags,
+                     cancellationToken);
+            }
+        }
+
+        return result;
+    }
+}
+
--- src/MechanicApplication/Common/Interfaces/ICachedQuery.cs
+++ src/MechanicApplication/Common/Interfaces/ICachedQuery.cs
@@ -6,7 +6,7 @@ namespace MechanicApplication.Common.Interfaces;
 public interface ICachedQuery
 {
     string CacheKey { get; }
-    string[] Tage { get; }
+    string[] Tags { get; }
     TimeSpan Expiration { get; }
 }
 public interface ICachedQuery<TResponse> : IRequest<TResponse>, ICachedQuery; 

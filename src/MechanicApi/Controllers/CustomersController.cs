86b0f2ab16838e16e4bbf95449fc34bb26ada779
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-18 09:33:52 +0200

    CreateCustomer

--- src/MechanicApi/Controllers/CustomersController.cs
+++ src/MechanicApi/Controllers/CustomersController.cs
@@ -1,7 +1,9 @@
 ﻿using Asp.Versioning;
+using MechanicApplication.Features.Customers.Commands.CreateCustomer;
 using MechanicApplication.Features.Customers.DTOs;
 using MechanicApplication.Features.Customers.Queries.GetCustoemerByID;
 using MechanicApplication.Features.Customers.Queries.GetCustomers;
+using MechanicContracts.Requests.Customers;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -27,15 +29,15 @@ public class CustomersController(ISender sender) : ApiBaseController
     [OutputCache(Duration = 60)]
     public async Task<ActionResult<List<CustomerDTO>>> GetCustomers(CancellationToken cancellationToken)
     {
-        var result = await sender.Send(new GetCustomersQuery(), cancellationToken); 
+        var result = await sender.Send(new GetCustomersQuery(), cancellationToken);
         return result.Match(
             Ok,
             Problem);
-        
+
     }
 
     [HttpGet("{customerId:guid}", Name = "GetCustomerById")]
-    [ProducesResponseType<CustomerDTO>( StatusCodes.Status200OK)]
+    [ProducesResponseType<CustomerDTO>(StatusCodes.Status200OK)]
     [ProducesResponseType<ProblemDetails>(StatusCodes.Status404NotFound)]
     [ProducesResponseType<ProblemDetails>(StatusCodes.Status500InternalServerError)]
     [EndpointSummary("Retrieves a customer by ID")]
@@ -49,6 +51,36 @@ public class CustomersController(ISender sender) : ApiBaseController
         var result = await sender.Send(new GetCustomerByIdQuery(customerId), cancellationToken);
         return result.Match(
             Ok,
+            Problem);
+    }
+    [HttpPost]
+    [ProducesResponseType<CustomerDTO>(StatusCodes.Status201Created)]
+    [ProducesResponseType<ProblemDetails>(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType<ProblemDetails>(StatusCodes.Status500InternalServerError)]
+    [EndpointSummary("Creates a new customer")]
+    [EndpointDescription("Creates a new customer ")]
+    [EndpointName("CreateCustomer")]
+    [MapToApiVersion("1.0")]
+    public async Task<ActionResult<CustomerDTO>> CreateCustomer([FromBody] CreateCustomerRequest command, CancellationToken cancellationToken)
+    {
+        var vehiclesCommands = command.Vehicles.Select(v => new CreateVehicleCommand(
+            v.Make,
+            v.Model,
+            v.Year,
+            v.LicensePlate)).ToList();
+
+        var result = await sender.Send(
+            new CreateCustomerCommand(
+                command.Name,
+                command.PhoneNumber,
+                command.Email,
+                vehiclesCommands)
+            , cancellationToken);
+
+        var currentVersion = HttpContext.GetRequestedApiVersion()!.ToString()?? "1.0";
+
+        return result.Match(
+            customer => CreatedAtRoute("GetCustomerById", new { version = currentVersion,  customerId = customer.CustomerId }, customer),
             Problem);
     }
 }

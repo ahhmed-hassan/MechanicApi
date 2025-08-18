4a785ac4d2bebbf7d84a9f7b655ef6a11e899980
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-18 09:00:34 +0200

    GetCustomerById

--- src/MechanicApi/Controllers/CustomersController.cs
+++ src/MechanicApi/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using Asp.Versioning;
 using MechanicApplication.Features.Customers.DTOs;
+using MechanicApplication.Features.Customers.Queries.GetCustoemerByID;
 using MechanicApplication.Features.Customers.Queries.GetCustomers;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
@@ -31,5 +32,23 @@ public class CustomersController(ISender sender) : ApiBaseController
             Ok,
             Problem);
         
+    }
+
+    [HttpGet("{customerId:guid}", Name = "GetCustomerById")]
+    [ProducesResponseType<CustomerDTO>( StatusCodes.Status200OK)]
+    [ProducesResponseType<ProblemDetails>(StatusCodes.Status404NotFound)]
+    [ProducesResponseType<ProblemDetails>(StatusCodes.Status500InternalServerError)]
+    [EndpointSummary("Retrieves a customer by ID")]
+    [EndpointDescription("Returns a customer associated with the current user by ID")]
+    [EndpointName("GetCustomerById")]
+    [MapToApiVersion("1.0")]
+    //It does not worth it to cce for just one customer
+    //[OutputCache(Duration =60, VaryByQueryKeys = new[] { "customerId" })]
+    public async Task<ActionResult<CustomerDTO>> GetCustomerById(Guid customerId, CancellationToken cancellationToken)
+    {
+        var result = await sender.Send(new GetCustomerByIdQuery(customerId), cancellationToken);
+        return result.Match(
+            Ok,
+            Problem);
     }
 }

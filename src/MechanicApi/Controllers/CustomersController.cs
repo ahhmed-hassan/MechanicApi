42d3dc4a8ab6e3b344e41966b4177e1ec908f6d9
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-18 08:50:59 +0200

    GetCustomers

---
+++ src/MechanicApi/Controllers/CustomersController.cs
@@ -0,0 +1,35 @@
+﻿using Asp.Versioning;
+using MechanicApplication.Features.Customers.DTOs;
+using MechanicApplication.Features.Customers.Queries.GetCustomers;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+
+namespace MechanicApi.Controllers;
+
+[Route("api/v{version:apiversion}/customers")]
+[ApiVersion("1.0")]
+[ApiController]
+[Authorize]
+public class CustomersController(ISender sender) : ApiBaseController
+
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(List<CustomerDTO>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    [EndpointSummary("Retrieves all the customers")]
+    [EndpointDescription("Returns all the custmers associated with the current user")]
+    [EndpointName("GetCustomers")]
+    [MapToApiVersion("1.0")]
+    [ProducesDefaultResponseType]
+    [OutputCache(Duration = 60)]
+    public async Task<ActionResult<List<CustomerDTO>>> GetCustomers(CancellationToken cancellationToken)
+    {
+        var result = await sender.Send(new GetCustomersQuery(), cancellationToken); 
+        return result.Match(
+            Ok,
+            Problem);
+        
+    }
+}

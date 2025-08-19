3dca92b5d6cb1085513d757ac58b8e6164b2a917
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-19 07:37:20 +0200

    AppSetitngs

---
+++ src/MechanicInfrastructure/Settings/AppSettings.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace MechanicInfrastructure.Settings;
+
+public sealed class AppSettings
+{
+    public TimeOnly OpeningTime { get; set; }
+    public TimeOnly ClosingTime { get; set; }
+    public int MaxSpots { get; set; }
+    public int MinimumAppointmentDurationInMinutes { get; set; }
+    public int LocalCacheExpirationInMins { get; set; }
+    public int DistributedCacheExpirationMins { get; set; }
+    public int DefaultPageNumber { get; set; }
+    public int DefaultPageSize { get; set; }
+    public int BookingCancellationThresholdMinutes { get; set; }
+    public int OverdueBookingCleanupFrequencyMinutes { get; set; }
+    public string CorsPolicyName { get; set; } = default!;
+    public string[] AllowedOrigins { get; set; } = default!;
+ 
+}
+

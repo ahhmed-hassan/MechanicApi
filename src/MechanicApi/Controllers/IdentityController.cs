f5db6c2cefac16292ce8e7efbe85b9a5713f5964
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-19 09:26:23 +0200

    current-user/claims Controller endpoing

---
+++ src/MechanicApi/Controllers/IdentityController.cs
@@ -0,0 +1,36 @@
+﻿using Asp.Versioning;
+using MechanicApplication.Features.Identity.DTOs;
+using MechanicApplication.Features.Identity.Queries.GetUserInfo;
+using MediatR;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace MechanicApi.Controllers;
+
+[Route("identity")]
+[ApiVersionNeutral]
+public sealed class IdentityController(ISender sender) : ApiBaseController
+{
+    /// <summary>
+    /// Returns user information for the currently authenticated user.
+    /// </summary>
+    /// <returns>An ActionResult containing the user information or an error.</returns>
+    [HttpGet("current-user/claims")]
+    [Authorize]
+    [ProducesResponseType(typeof(AppUserDTO), StatusCodes.Status200OK)]
+    [ProducesResponseType<ProblemDetails>(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType<ProblemDetails>(StatusCodes.Status500InternalServerError)]
+    [EndpointDescription("Get current user claims Returns the claims of the currently authenticated user.")]
+    [EndpointSummary("Get the current authenticated user's claims.")]
+    [EndpointName("GetCurrentUserClaims")]
+    public async Task<IActionResult> GetCurrentUserInfor(CancellationToken cancellationToken)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var result = await sender.Send(new GetUserByIdQuery(userId), cancellationToken);
+        return result.Match(
+            Ok,
+            Problem);
+    }
+}

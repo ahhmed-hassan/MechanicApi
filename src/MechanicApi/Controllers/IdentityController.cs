446722bac887516502b9c566463f65e7a1b149a8
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-19 09:50:29 +0200

    Refresh token Cotroller endpoint

--- src/MechanicApi/Controllers/IdentityController.cs
+++ src/MechanicApi/Controllers/IdentityController.cs
@@ -3,12 +3,14 @@ using MechanicApplication.Features.Identity.DTOs;
 using MechanicApplication.Features.Identity.Queries;
 using MechanicApplication.Features.Identity.Queries.GenerateTokens;
 using MechanicApplication.Features.Identity.Queries.GetUserInfo;
+using MechanicApplication.Features.Identity.Queries.RefreshTokens;
 using MediatR;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using System.Security.Claims;
+using System.Transactions;
 
 namespace MechanicApi.Controllers;
 
@@ -20,6 +22,7 @@ public sealed class IdentityController(ISender sender) : ApiBaseController
     /// <summary>
     /// Generates a token for the user.
     /// 
+    /// 
     [HttpPost("token/generate")]
     [ProducesResponseType<TokenResponse>(StatusCodes.Status200OK)]
     [ProducesResponseType<ProblemDetails>(StatusCodes.Status400BadRequest)]
@@ -28,10 +31,25 @@ public sealed class IdentityController(ISender sender) : ApiBaseController
     [EndpointDescription("Authenticates a user using provided credentials and returns a JWT token pair.")]
     [EndpointName("GenerateToken")]
  
-    public async Task<ActionResult<TokenResponse>> GenerateTokenQuery([FromBody] GenerateTokenQuery request, CancellationToken cancellation)
+    public async Task<ActionResult<TokenResponse>> GenerateTokenQuery([FromBody] GenerateTokenQuery request,
+        CancellationToken cancellation)
     {
         var result = await sender.Send(request, cancellation);
         return result.Match(Ok,Problem);
+    }
+    [HttpPost("token/refresh-token")]
+    [ProducesResponseType<TokenResponse>( StatusCodes.Status200OK)]
+    [ProducesResponseType<ProblemDetails>( StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    [EndpointSummary("Refreshes access token using a valid refresh token.")]
+    [EndpointDescription("Exchanges an expired access token and a valid refresh token for a new token pair.")]
+    [EndpointName("RefreshToken")]
+    public async Task<ActionResult<TokenResponse>> RefreshTokenQuery(
+        [FromBody] RefreshTokenQuery request,
+        CancellationToken cancellation)
+    {
+        var result = await sender.Send(request, cancellation);
+        return result.Match(Ok, Problem);
     }
     /// <summary>
     /// Returns user information for the currently authenticated user.

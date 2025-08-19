41cc53271aafafef1f3d03b395ceabfd31a1c314
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-19 08:15:17 +0200

    GetUserByIdQuery

---
+++ src/MechanicApplication/Features/Identity/Queries/GetUserInfo/GetUserByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using ErrorOr;
+using MechanicApplication.Features.Identity.DTOs;
+using MediatR;
+
+namespace MechanicApplication.Features.Identity.Queries.GetUserInfo;
+
+public sealed record GetUserByIdQuery(string? UserId) : IRequest<ErrorOr<AppUserDTO>>;
+
+
---
+++ src/MechanicApplication/Features/Identity/Queries/GetUserInfo/GetUserByIdQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using ErrorOr;
+using MechanicApplication.Common.Interfaces;
+using MechanicApplication.Features.Identity.DTOs;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace MechanicApplication.Features.Identity.Queries.GetUserInfo;
+
+public sealed class GetUserByIdQueryHandler(
+    ILogger<GetUserByIdQueryHandler> logger,
+    IIdenttiyService identtiyService)
+    : IRequestHandler<GetUserByIdQuery, ErrorOr<AppUserDTO>>
+{
+    private readonly ILogger<GetUserByIdQueryHandler> _logger = logger;
+    private readonly IIdenttiyService _identtiyService = identtiyService;
+
+    public async Task<ErrorOr<AppUserDTO>> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+    {
+        var user = await _identtiyService.GetUserByIdAsync(request.UserId);
+         user.SwitchFirst(
+            user => { },
+            error =>  _logger.LogWarning("Failed to retrieve user with ID {UserId}: {Error}", request.UserId, error.Description) 
+            );
+        return user;
+
+    }
+}

a5fde714486219ec2b34fd0473d5a59095ee7a36
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-19 09:09:06 +0200

    GenerateTokenQuery

---
+++ src/MechanicApplication/Features/Identity/Queries/GenerateTokens/GenerateTokenQuery.cs
@@ -0,0 +1,7 @@
+﻿using ErrorOr;
+using MediatR;
+
+namespace MechanicApplication.Features.Identity.Queries.GenerateTokens;
+
+public sealed record GenerateTokenQuery(string Email , string Password): IRequest<ErrorOr<TokenResponse>>;
+

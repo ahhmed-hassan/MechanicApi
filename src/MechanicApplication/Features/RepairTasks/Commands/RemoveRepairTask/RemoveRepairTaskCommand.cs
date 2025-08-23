0326cb8b438e7dc9004972785769a71cd1236f98
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-23 20:33:32 +0200

    RemoveRepairTaskCommand and Handler

---
+++ src/MechanicApplication/Features/RepairTasks/Commands/RemoveRepairTask/RemoveRepairTaskCommand.cs
@@ -0,0 +1,16 @@
+﻿using ErrorOr;
+using FluentValidation;
+using MediatR;
+
+namespace MechanicApplication.Features.RepairTasks.Commands.RemoveRepairTask;
+
+public sealed record RemoveRepairTaskCommand(Guid RepairTaskId) : IRequest<ErrorOr<Deleted>>;
+
+public class RemoveRepairTaskCommandValidator : AbstractValidator<RemoveRepairTaskCommand>
+{
+    public RemoveRepairTaskCommandValidator()
+    {
+        RuleFor(x => x.RepairTaskId)
+            .NotEmpty().WithMessage("RepairTaskId is required.");
+    }
+}

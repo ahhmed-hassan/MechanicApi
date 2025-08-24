948e7803f9961af62860927b231ae0f4d7b7969c
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-24 16:08:21 +0200

    Adding the controller

---
+++ src/MechanicApi/Controllers/WorkOrderController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MechanicApi.Controllers
+{
+    [Authorize]
+    [Route("api/v{version:apiVersion}/workorders")]
+    [ApiVersion("1.0")]
+    public class WorkOrderController : ApiBaseController
+    {
+        
+    }
+}

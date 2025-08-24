7be2ab32b488d9c0bb3e80e4d2914643951da2a9
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-24 06:55:37 +0200

    UpdateRepairTask {Command, Handler, Validator}

---
+++ src/MechanicApplication/Features/RepairTasks/Commands/UpdateRepairTask/UpdateRepairTaskCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace MechanicApplication.Features.RepairTasks.Commands.UpdateRepairTask;
+
+public class UpdateRepairTaskCommandValidator : AbstractValidator<UpdateRepairTaskCommand>
+{
+    public UpdateRepairTaskCommandValidator()
+    {
+        RuleFor(x => x.RepairTaskId)
+            .NotEmpty().WithMessage("Repair Task ID is required required.");
+
+        RuleFor(x => x.Name)
+            .MaximumLength(100).WithMessage("Name must not exceed 500 characters.");
+
+        RuleFor(x => x.LaborCost)
+            .InclusiveBetween(1,10000).WithMessage("Labor cost must be greater than 0.")
+            .LessThanOrEqualTo(10000).WithMessage("Labor cost must not exceed 10,000.");
+
+        RuleFor(x => x.EstimatedDurationInMins)
+            .IsInEnum().WithMessage("Invalid duration selected");
+
+        RuleForEach(x => x.CommandParts).SetValidator(new UpdateRepairTaskPartCommandValidator());
+    }
+
+}
---
+++ src/MechanicApplication/Features/RepairTasks/Commands/UpdateRepairTask/UpdateRepairTaskPartCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace MechanicApplication.Features.RepairTasks.Commands.UpdateRepairTask;
+
+internal class UpdateRepairTaskPartCommandValidator : AbstractValidator<UpdateRepairTaskPartCommand>
+{
+    public UpdateRepairTaskPartCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Part name is required.")
+            .MaximumLength(100).WithMessage("Part name must not exceed 100 characters.");
+        RuleFor(x => x.Cost)
+            .GreaterThan(0).WithMessage("Part cost must be greater than 0.")
+            .LessThanOrEqualTo(1000).WithMessage("Part cost must not exceed 1000.");
+        RuleFor(x => x.Quantity)
+            .GreaterThan(0).WithMessage("Part quantity must be greater than 0.")
+            .LessThanOrEqualTo(10).WithMessage("Part quantity must not exceed 10.");
+    }
+}

221aa3301904d36727cdb607dbbf7a0940e382d3
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-24 16:15:19 +0200

    Command And HAndler bliueprint

---
+++ src/MechanicApplication/Features/WorkOrders/Commands/RelocateWorkOrder/RelocateWorkOrderCommand.cs
@@ -0,0 +1,13 @@
+﻿using ErrorOr;
+using MechanicDomain.WorkOrders.Enums;
+using MediatR;
+
+namespace MechanicApplication.Features.WorkOrders.Commands.RelocateWorkOrder
+{
+    public sealed record RelocateWorkOrderCommand(
+        Guid Id, 
+        DateTimeOffset NewStartAt, 
+        Spot NewSpot
+        ) : IRequest<ErrorOr<Updated>>;
+    
+}
---
+++ src/MechanicApplication/Features/WorkOrders/Commands/RelocateWorkOrder/RelocateWorkOrderCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿
+using ErrorOr;
+using MechanicApplication.Common.Interfaces;
+using MediatR;
+
+namespace MechanicApplication.Features.WorkOrders.Commands.RelocateWorkOrder
+{
+    public class RelocateWorkOrderCommandHandler(IAppDbContext context) : IRequestHandler<RelocateWorkOrderCommand, ErrorOr<Updated>>
+
+    {
+        public Task<ErrorOr<Updated>> Handle(RelocateWorkOrderCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

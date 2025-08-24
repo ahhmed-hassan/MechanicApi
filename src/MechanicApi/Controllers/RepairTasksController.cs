6b40836ffae776ee5a5e8fccf7c3e5d66b7f61ab
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-24 08:41:37 +0200

    UpdateRepairTask Endpoint

--- src/MechanicApi/Controllers/RepairTasksController.cs
+++ src/MechanicApi/Controllers/RepairTasksController.cs
@@ -2,6 +2,7 @@
 using ErrorOr;
 using MechanicApplication.Features.RepairTasks.Commands.CreateRepairTask;
 using MechanicApplication.Features.RepairTasks.Commands.RemoveRepairTask;
+using MechanicApplication.Features.RepairTasks.Commands.UpdateRepairTask;
 using MechanicApplication.Features.RepairTasks.DTOs;
 using MechanicApplication.Features.RepairTasks.Queries.GetRepairTasks;
 using MechanicApplication.Features.RepairTasks.Queries.GetRepiarTaskById;
@@ -64,6 +65,32 @@ public sealed class RepairTasksController(ISender sender) : ApiBaseController
             );
 
     }
+    [HttpPut("{repairTaskId: guid}")]
+    [Authorize(Roles = nameof(Role.Manager))]
+    [ProducesResponseType<RepairTaskDTO>(StatusCodes.Status204NoContent)]
+    [ProducesResponseType<ValidationProblemDetails>(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType<ProblemDetails>(StatusCodes.Status404NotFound)]
+    [ProducesResponseType<ProblemDetails>(StatusCodes.Status500InternalServerError)]
+    [EndpointSummary("Updates an existing repait task")]
+    [EndpointDescription("Updates a repair task and its associated parts")]
+    [EndpointName("UpdateRepairTask")]
+    public async Task<IActionResult> Update(Guid repairTaskId, [FromBody] UpdateRepairTaskRequest request, CancellationToken cancellationToken)
+    {
+        var commandParts = request.Parts
+            .ConvertAll(p => new UpdateRepairTaskPartCommand(PartId: p.PartId, Name: p.Name, Cost: p.Cost, Quantity: p.Quantity));
+        var updateCommand = new UpdateRepairTaskCommand(
+            repairTaskId,
+            request.Name,
+            request.LaborCost,
+            (RepairDurationInMinutes)request.EstimatedDurationInMins,
+            commandParts
+            );
+        var result = await sender.Send(updateCommand, cancellationToken);
+        return result.Match(response => Ok(response), Problem);
+
+
+    }
+
     [HttpGet("{repairTaskId:guid}", Name = nameof(GetById))]
     [ProducesResponseType<RepairTaskDTO>(StatusCodes.Status200OK)]
     [ProducesResponseType<ProblemDetails>(StatusCodes.Status404NotFound)]
@@ -74,8 +101,8 @@ public sealed class RepairTasksController(ISender sender) : ApiBaseController
     [MapToApiVersion("1.0")]
     public async Task<IActionResult> GetById(Guid repairTaskId, CancellationToken cancellationToken)
     {
-       var result = await sender.Send(new GetRepairTaskByIdQuery(repairTaskId), cancellationToken);
-         return result.Match(Ok, Problem);
+        var result = await sender.Send(new GetRepairTaskByIdQuery(repairTaskId), cancellationToken);
+        return result.Match(Ok, Problem);
     }
 
     [HttpDelete("{repairTaskId:guid}")]
---
+++ src/MechanicContracts/Requests/RepairTasks/UpdateRepairTaskRequest.cs
@@ -0,0 +1,20 @@
+﻿using MechanicContracts.Shared;
+using System.ComponentModel.DataAnnotations;
+
+namespace MechanicContracts.Requests.RepairTasks;
+
+public class UpdateRepairTaskRequest
+{
+    [Required(ErrorMessage = "Task name is requires")]
+    public string Name  { get; set;} = string.Empty;
+    [Required(ErrorMessage = "Labor cost is required.")]
+    [Range(1, 10000, ErrorMessage = "Labor cost must be between 1 and 10,000.")]
+    public decimal LaborCost { get; set; }
+
+    [Required(ErrorMessage = "Estimated duration is required.")]
+    public RepairDurationInMinutes EstimatedDurationInMins { get; set; }
+
+    [MinLength(1, ErrorMessage = "At least one part is required.")]
+    //[ValidateComplexType]
+    public List<UpdateRepairTaskPartRequest> Parts { get; set; } = [];
+}

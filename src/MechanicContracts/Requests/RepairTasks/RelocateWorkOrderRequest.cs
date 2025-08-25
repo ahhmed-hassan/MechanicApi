0aa2ec6a9e8b10c6fa027d053fb3abfb68206de7
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-25 18:42:24 +0200

    ReuqestAndApi

---
+++ src/MechanicContracts/Requests/RepairTasks/RelocateWorkOrderRequest.cs
@@ -0,0 +1,8 @@
+﻿
+using MechanicContracts.Shared;
+using MechanicDomain.WorkOrders.Enums;
+
+namespace MechanicContracts.Requests.RepairTasks;
+
+public sealed record RelocateWorkOrderRequest(DateTimeOffset NewStart, Spot newSpot);
+

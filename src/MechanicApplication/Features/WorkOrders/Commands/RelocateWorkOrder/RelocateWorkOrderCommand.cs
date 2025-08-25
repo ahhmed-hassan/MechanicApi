bd3ef981d7fdc4bcfbb2ef6bba905128ff5bc2ce
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-08-25 18:25:58 +0200

    Validator

--- src/MechanicApplication/Features/WorkOrders/Commands/RelocateWorkOrder/RelocateWorkOrderCommand.cs
+++ src/MechanicApplication/Features/WorkOrders/Commands/RelocateWorkOrder/RelocateWorkOrderCommand.cs
@@ -1,13 +1,28 @@
 ﻿using ErrorOr;
+using FluentValidation;
 using MechanicDomain.WorkOrders.Enums;
 using MediatR;
 
-namespace MechanicApplication.Features.WorkOrders.Commands.RelocateWorkOrder
+namespace MechanicApplication.Features.WorkOrders.Commands.RelocateWorkOrder;
+
+public sealed record RelocateWorkOrderCommand(
+    Guid Id, 
+    DateTimeOffset NewStartAt, 
+    Spot NewSpot
+    ) : IRequest<ErrorOr<Updated>>;
+
+public sealed class RelocateWorkorderCommandValidator : AbstractValidator<RelocateWorkOrderCommand>
 {
-    public sealed record RelocateWorkOrderCommand(
-        Guid Id, 
-        DateTimeOffset NewStartAt, 
-        Spot NewSpot
-        ) : IRequest<ErrorOr<Updated>>;
-    
+    public RelocateWorkorderCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty();
+
+        RuleFor(x => x.NewStartAt)
+            .GreaterThan(DateTimeOffset.UtcNow)
+            .WithMessage("New start time must be in the future");
+
+        RuleFor(x => x.NewSpot)
+            .IsInEnum(); 
+    }
 }

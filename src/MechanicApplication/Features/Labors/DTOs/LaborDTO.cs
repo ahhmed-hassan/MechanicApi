f3217e511e9db2e7d9e51487a412ae7be266a182
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-11-13 08:05:44 +0100

    LAborDTO

---
+++ src/MechanicApplication/Features/Labors/DTOs/LaborDTO.cs
@@ -0,0 +1,19 @@
+﻿
+using MechanicDomain.Employees;
+
+namespace MechanicApplication.Features.Labors.DTOs;
+
+public class LaborDTO
+{
+    public Guid LaborId { get; set; }
+    public string Name { get; set; } = string.Empty;
+
+    public static LaborDTO fromLabor (Employee labor) =>
+        new LaborDTO
+        {
+            LaborId = labor.Id,
+            Name = labor.FullName
+        };
+    static public List<LaborDTO> fromLabors (IEnumerable<Employee> labors) =>
+        labors.Select(fromLabor).ToList();
+}

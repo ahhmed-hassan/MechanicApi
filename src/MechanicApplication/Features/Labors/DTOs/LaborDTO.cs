7d36c1bd7ed0904a5fd272387827aea568dd69d0
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-11-13 08:18:09 +0100

    GetLaobrs Query and its handler

--- src/MechanicApplication/Features/Labors/DTOs/LaborDTO.cs
+++ src/MechanicApplication/Features/Labors/DTOs/LaborDTO.cs
@@ -8,12 +8,12 @@ public class LaborDTO
     public Guid LaborId { get; set; }
     public string Name { get; set; } = string.Empty;
 
-    public static LaborDTO fromLabor (Employee labor) =>
+    public static LaborDTO fromEmployee (Employee labor) =>
         new LaborDTO
         {
             LaborId = labor.Id,
             Name = labor.FullName
         };
-    static public List<LaborDTO> fromLabors (IEnumerable<Employee> labors) =>
-        labors.Select(fromLabor).ToList();
+    static public List<LaborDTO> fromEmployees (IEnumerable<Employee> labors) =>
+        labors.Select(fromEmployee).ToList();
 }

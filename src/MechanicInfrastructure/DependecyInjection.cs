d0d7ed554a4b518845a3efcf904ecc05d7b33335
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-11-27 00:25:23 +0100

    DependencyInjection blueprint

---
+++ src/MechanicInfrastructure/DependecyInjection.cs
@@ -0,0 +1,40 @@
+﻿
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.Extensions.Configuration;
+
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+
+public static  class DependecyInjection
+{
+    public static IServiceCollection AddInfrastructure (this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        #region  Add Database Context
+
+        #endregion
+        // Add infrastructure services here (e.g., database context, repositories, etc.)
+        #region Authentication and Authorization
+
+        #endregion
+
+        #region Caching
+
+        #endregion
+
+        //TODO : Token Provieder
+
+        //TODO: WorkOrderPolicy
+
+        //TODO: Notification Service
+
+        //TODO: IWorkOrderNotifier
+
+        //TODO : IInvoicePDFGenerator
+
+        //TODO: OverdueBookingCleanupService
+        return services;
+    }
+}

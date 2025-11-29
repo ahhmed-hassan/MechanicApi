07624ad290d5e260db7256acfadd3ebef5528083
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-11-29 14:57:01 +0100

    Application Layer : UpdateWorkOrderstate command and its handler and validator

---
+++ src/MechanicApplication/Features/WorkOrders/Commands/UpdateWorkOrderState/UpdateWorkOrderCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿
+using FluentValidation;
+
+namespace MechanicApplication.Features.WorkOrders.Commands.UpdateWorkOrderState;
+
+public sealed class UpdateWorkOrderStateCommandValidator : AbstractValidator<UpdateWorkOrderState>
+{
+    public UpdateWorkOrderStateCommandValidator()
+    {
+        RuleFor(x => x.State)
+            .IsInEnum()
+            .WithErrorCode("WokrOrder.InvalidState")
+            .WithMessage("Invalid work order state.");
+    }
+}

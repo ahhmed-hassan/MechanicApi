ee9d802f3e38ff698c73bfd2f3c3a2a5a92b918e
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-11-30 13:00:10 +0100

    USer factory for tests

---
+++ tests/MechanicApi.Tests.Common/Security/TestCurrentUser.cs
@@ -0,0 +1,16 @@
+﻿using MechanicApplication.Common.Interfaces;
+using MechanicInfrastructure.Identity;
+
+namespace MechanicShop.Tests.Common.Security;
+
+public class TestCurrentUser : IUser
+{
+    private AppUser? _currentUser;
+
+    public void Returns(AppUser currentUser)
+    {
+        _currentUser = currentUser;
+    }
+
+    public string? Id => _currentUser!.Id ?? UserFactory.DummyUser.Id;
+}

7306863b35d37612c19ef297d4a063fd058a7bcf
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-11-30 01:53:49 +0100

    Registering Authorization dependecies

--- src/MechanicInfrastructure/DependecyInjection.cs
+++ src/MechanicInfrastructure/DependecyInjection.cs
@@ -1,4 +1,7 @@
 ﻿
+using MechanicDomain.Identity;
+using MechanicInfrastructure.Identity.Policies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Microsoft.Extensions.Configuration;
@@ -7,17 +10,27 @@ using Microsoft.Extensions.Configuration;
 namespace Microsoft.Extensions.DependencyInjection;
 
 
-public static  class DependecyInjection
+public static class DependecyInjection
 {
-    public static IServiceCollection AddInfrastructure (this IServiceCollection services,
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services,
         IConfiguration configuration)
     {
         #region  Add Database Context
 
         #endregion
         // Add infrastructure services here (e.g., database context, repositories, etc.)
-        #region Authentication and Authorization
+        #region Identity 
 
+        #endregion
+        #region Authentication and Authorization
+        services.AddScoped<IAuthorizationHandler, LaborAssignedHandler>();
+
+        services.AddAuthorizationBuilder()
+            .AddPolicy(MechanicApplication.Common.Constants.AuhtorizationConstants.SelfScopedWorkedOrderAccess,
+            policy => policy.Requirements.Add(new LaborAssigned()))
+
+            .AddPolicy(MechanicApplication.Common.Constants.AuhtorizationConstants.ManagerOnly,
+                       policy => policy.RequireRole(nameof(Role.Manager)));
         #endregion
 
         #region Caching

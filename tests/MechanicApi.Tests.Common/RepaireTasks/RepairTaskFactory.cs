cd96fe118104163be7c7da83cd10d7fa3d4e6daa
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-11-30 11:41:10 +0100

    RepairTasks Factory

---
+++ tests/MechanicApi.Tests.Common/RepaireTasks/RepairTaskFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using ErrorOr;
+using MechanicDomain.RepairTasks;
+using MechanicDomain.RepairTasks.Enums;
+using MechanicDomain.RepairTasks.Parts;
+
+namespace MechanicShop.Tests.Common.RepaireTasks;
+
+public static class RepairTaskFactory
+{
+    public static ErrorOr<RepairTask> CreateRepairTask(
+        Guid? id = null,
+        string name = "Brake Inspection",
+        decimal laborCost = 100m,
+        RepairDurationInMinutes repairDurationInMinutes = RepairDurationInMinutes.Min30,
+        List<Part>? parts = null)
+    {
+        return RepairTask.Create(
+            id ?? Guid.NewGuid(),
+            name,
+            laborCost,
+            repairDurationInMinutes,
+            parts ?? new List<Part> { Part.Create(Guid.NewGuid(), "Brake Pads", 50m, 1).Value });
+    }
+}

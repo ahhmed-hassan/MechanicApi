c68ecd1583ec943fc8f43bff9d42ca0231613ed4
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-12-20 18:31:55 +0100

    Partconfiguration

---
+++ src/MechanicInfrastructure/Data/Configrations/Partconfiguration.cs
@@ -0,0 +1,31 @@
+﻿
+using MechanicDomain.RepairTasks.Parts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MechanicInfrastructure.Data.Configrations;
+
+public class Partconfiguration : IEntityTypeConfiguration<Part>
+{
+    
+
+    public void Configure(EntityTypeBuilder<Part> builder)
+    {
+        builder.HasKey(p => p.Id);
+
+        builder.Property(p=> p.Id)
+            .ValueGeneratedNever();
+
+        builder.Property(p => p.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(p => p.Cost)
+            .IsRequired()
+            .HasColumnType("decimal(18,2)");
+
+        builder.Property(p => p.Quantity)
+            .IsRequired();
+      
+    }
+}

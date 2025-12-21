046b4fbf25f93f12a0fc428adcd90633227f666c
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-12-21 12:28:12 +0100

    Confiugration for the vehicle

---
+++ src/MechanicInfrastructure/Data/Configrations/EntityTypeBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using MechanicDomain.Abstractions;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace MechanicInfrastructure.Data.Configrations;
+
+public static class EntityTypeBuilderExtensions
+{
+    public static void ApplyAuditableConfiguration<T>(this EntityTypeBuilder<T> builder)
+        where T : AuditableEntity
+    {
+        builder.HasKey(e => e.Id);
+        builder.Property(e => e.Id)
+            .ValueGeneratedNever();
+    }
+}

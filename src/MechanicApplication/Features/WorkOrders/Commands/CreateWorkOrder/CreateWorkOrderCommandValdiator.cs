414d485f4911557ce84504e1931883d9e8441d73
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-12-28 23:32:50 +0100

    PAssing the test `Validate_ShouldReturnError_WhenVehicleIdIsEmpty`

--- src/MechanicApplication/Features/WorkOrders/Commands/CreateWorkOrder/CreateWorkOrderCommandValdiator.cs
+++ src/MechanicApplication/Features/WorkOrders/Commands/CreateWorkOrder/CreateWorkOrderCommandValdiator.cs
@@ -1,8 +1,17 @@
 ﻿using FluentValidation;
+using MechanicDomain.Customers.Vehicles;
+using System.Data;
 
 namespace MechanicApplication.Features.WorkOrders.Commands.CreateWorkOrder;
 
 public sealed class CreateWorkOrderCommandValdiator
     : AbstractValidator<CreateWorkOrderCommand>
 {
+    public CreateWorkOrderCommandValdiator()
+    {
+     RuleFor(x=> x.VehicleId)
+            .NotEmpty()
+            .WithMessage("VehicleId must not be empty");
+
+    }
 }

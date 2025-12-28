6ed0dcd86c0fc91bb7a72981c829e15559c7f68c
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-12-28 23:36:31 +0100

    Passing all the tests of `CreateWorkOrderCommandValidatorTests`

--- src/MechanicApplication/Features/WorkOrders/Commands/CreateWorkOrder/CreateWorkOrderCommandValdiator.cs
+++ src/MechanicApplication/Features/WorkOrders/Commands/CreateWorkOrder/CreateWorkOrderCommandValdiator.cs
@@ -9,9 +9,21 @@ public sealed class CreateWorkOrderCommandValdiator
 {
     public CreateWorkOrderCommandValdiator()
     {
-     RuleFor(x=> x.VehicleId)
+        RuleFor(x => x.VehicleId)
+               .NotEmpty()
+               .WithMessage("VehicleId must not be empty");
+
+        RuleFor(x => x.RepairTasksIds)
+                .NotEmpty()
+                .WithMessage("RepairTasksIds must contain at least one item");
+        
+        RuleFor(x => x.StartAt)
+            .GreaterThan(DateTimeOffset.Now)
+            .WithMessage("StartAt must be in the future");
+
+        RuleFor(x => x.LaborId)
             .NotEmpty()
-            .WithMessage("VehicleId must not be empty");
+            .WithMessage("LaborId must not be empty");
 
     }
 }

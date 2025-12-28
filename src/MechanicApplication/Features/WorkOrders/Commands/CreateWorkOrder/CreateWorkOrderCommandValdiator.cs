de96bcc2ccb56d1a5f3634e1e0fcd4a8cf78eddf
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-12-28 23:19:59 +0100

    Adding the CommandValidator Class and its test class

---
+++ src/MechanicApplication/Features/WorkOrders/Commands/CreateWorkOrder/CreateWorkOrderCommandValdiator.cs
@@ -0,0 +1,8 @@
+﻿using FluentValidation;
+
+namespace MechanicApplication.Features.WorkOrders.Commands.CreateWorkOrder;
+
+public sealed class CreateWorkOrderCommandValdiator
+    : AbstractValidator<CreateWorkOrderCommand>
+{
+}
---
+++ tests/MechanicShop.Application.subcutaneoustests/Features/WorkOrders/Commands/CreateWorkOrder/CreateWorkOrderValidatorTests.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Xunit;
+using MechanicApplication.Features.WorkOrders.Commands.CreateWorkOrder;
+namespace MechanicApi.Application.subcutaneoustests.Features.WorkOrders.Commands.CreateWorkOrder;
+
+public class CreateWorkOrderValidatorTests
+{
+    private readonly CreateWorkOrderCommandValdiator _validator = new CreateWorkOrderCommandValdiator();
+
+
+}

283e0866a597c3f7232e23859ebc566f3ef36441
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-12-28 23:24:24 +0100

    Adding the Test Validate_ShouldReturnError_WhenVehicleIdIsEmpty

--- tests/MechanicShop.Application.subcutaneoustests/Features/WorkOrders/Commands/CreateWorkOrder/CreateWorkOrderValidatorTests.cs
+++ tests/MechanicShop.Application.subcutaneoustests/Features/WorkOrders/Commands/CreateWorkOrder/CreateWorkOrderValidatorTests.cs
@@ -1,11 +1,30 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using MechanicApplication.Features.WorkOrders.Commands.CreateWorkOrder;
+using MechanicDomain.WorkOrders.Enums;
+using System.ComponentModel.DataAnnotations;
 using Xunit;
-using MechanicApplication.Features.WorkOrders.Commands.CreateWorkOrder;
 namespace MechanicApi.Application.subcutaneoustests.Features.WorkOrders.Commands.CreateWorkOrder;
 
 public class CreateWorkOrderValidatorTests
 {
     private readonly CreateWorkOrderCommandValdiator _validator = new CreateWorkOrderCommandValdiator();
 
+    [Fact]
+    public void Validate_ShouldReturnError_WhenVehicleIdIsEmpty()
+    {
+        // Arrange
+        var command = new CreateWorkOrderCommand(
+          Spot: Spot.A,
+          VehicleId: Guid.Empty,
+          StartAt: DateTime.UtcNow.AddHours(1),
+          RepairTasksIds: [Guid.NewGuid()],
+          LaborId: Guid.NewGuid()
+          );
 
+        // Act
+        var result = _validator.Validate(command);
+        // Assert
+        Assert.False(result.IsValid);
+        Assert.Contains(result.Errors, e => e.PropertyName == nameof(CreateWorkOrderCommand.VehicleId) &&
+                                           e.ErrorMessage.Contains("VehicleId must not be empty"));
+    }
 }

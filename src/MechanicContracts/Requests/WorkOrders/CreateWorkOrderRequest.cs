186a1586c44fff2a286e092558e40dc3ef868c0a
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-12-29 17:10:18 +0100

    Adding the `CreateWorkOrderRequest`

---
+++ src/MechanicContracts/Requests/WorkOrders/CreateWorkOrderRequest.cs
@@ -0,0 +1,28 @@
+﻿
+
+using MechanicContracts.Shared;
+using System.ComponentModel.DataAnnotations;
+
+namespace MechanicContracts.Requests.WorkOrders;
+
+public sealed class CreateWorkOrderRequest
+{
+   
+
+    [Required(ErrorMessage ="Spot is Required")]
+    [Range(0,3, ErrorMessage ="The Range is [0,3]")]
+    public Spot Spot { get; set; }
+
+    [Required(ErrorMessage = "VehicleId is required")]
+    public Guid VehicleId { get; set; }
+
+    [Required(ErrorMessage = "LaborIs is required")]
+    public Guid LaborId { get; set;  }
+
+    [MinLength(1, ErrorMessage = "Work Order should refernce at least one specific repair task")]
+    public List<Guid> RepairTasksIds { get; set; } = [];
+
+    [Required(ErrorMessage = "Start time is required")]
+    public DateTimeOffset StartAtUtc { get; set; }
+
+}

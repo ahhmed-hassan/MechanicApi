8fd0760ba75c76a71cdb58e59d5f75f24714b2d0
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2026-01-01 15:59:36 +0100

    feat: add validator for `UpdateWorkOrderRepairTasksCommandValidator` This ensures the command cannot be instinciated with either a an empty `workOrderId` or empty `RepairTasksIds`, letting the handler focusing only on the logic itself not data validation

---
+++ tests/MechanicShop.Application.subcutaneoustests/Features/WorkOrders/Commands/UpdateWorkOrderRepairTasks/UpdateWorkOrderRepairTasksCommandValidator.cs
@@ -0,0 +1,57 @@
+﻿
+
+using MechanicApplication.Features.WorkOrders.Commands.UpdateWorkOrderRepairTasks;
+using Xunit;
+
+namespace MechanicApi.Application.subcutaneoustests.Features.WorkOrders.Commands.UpdateWorkOrderRepairTasks;
+
+public class UpdateWorkOrderRepairTasksCommandValidatorTests
+{
+    private readonly UpdateWorkOrderRepairTasksCommandValidator _validator;
+
+    public UpdateWorkOrderRepairTasksCommandValidatorTests()
+    {
+        _validator = new UpdateWorkOrderRepairTasksCommandValidator();
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_WorkOrderId_Is_Empty()
+    {
+        var command = new UpdateWorkOrderRepairTasksCommand(
+            WorkOrderId: Guid.Empty,
+            RepairTasksIds: [Guid.NewGuid()]);
+
+        var result = _validator.Validate(command);
+
+        Assert.False(result.IsValid);
+        Assert.Contains(result.Errors, e => e.PropertyName == nameof(command.WorkOrderId));
+        Assert.Contains(result.Errors, e => e.ErrorCode == "WorkOrderId_Required");
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_RepairTaskIds_Is_Empty()
+    {
+        var command = new UpdateWorkOrderRepairTasksCommand(
+            WorkOrderId: Guid.NewGuid(),
+            RepairTasksIds: []);
+
+        var result = _validator.Validate(command);
+
+        Assert.False(result.IsValid);
+        Assert.Contains(result.Errors, e => e.PropertyName == nameof(command.RepairTasksIds));
+        Assert.Contains(result.Errors, e => e.ErrorCode == "RepairTasks_Required");
+    }
+
+    [Fact]
+    public void Should_Not_Have_Error_When_Command_Is_Valid()
+    {
+        var command = new UpdateWorkOrderRepairTasksCommand(
+            WorkOrderId: Guid.NewGuid(),
+            RepairTasksIds: [Guid.NewGuid(), Guid.NewGuid()]);
+
+        var result = _validator.Validate(command);
+
+        Assert.True(result.IsValid);
+        Assert.Empty(result.Errors);
+    }
+}

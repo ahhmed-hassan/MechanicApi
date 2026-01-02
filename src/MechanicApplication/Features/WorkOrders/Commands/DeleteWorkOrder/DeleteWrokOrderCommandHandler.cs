7bde0b712b83036b61fb1e6ddedea05b837d934e
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2026-01-02 21:04:30 +0100

    Blue prints for the the `DeleteWorkOrderCommand`, its validator and handler.
    No implmentation yet.

---
+++ src/MechanicApplication/Features/WorkOrders/Commands/DeleteWorkOrder/DeleteWrokOrderCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using ErrorOr;
+using MediatR;
+
+namespace MechanicApplication.Features.WorkOrders.Commands.DeleteWorkOrder;
+
+public sealed class DeleteWrokOrderCommandHandler
+    (
+    )
+    : IRequestHandler<DeleteWorkOrderCommand, ErrorOr<Deleted>>
+{
+    public Task<ErrorOr<Deleted>> Handle(DeleteWorkOrderCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/MechanicApplication/Features/WorkOrders/Commands/DeleteWorkOrder/DeleteWrokOrderCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace MechanicApplication.Features.WorkOrders.Commands.DeleteWorkOrder;
+
+public sealed class DeleteWrokOrderCommandValidator : AbstractValidator<DeleteWorkOrderCommand>
+{
+    public DeleteWrokOrderCommandValidator()
+    {
+        RuleFor(x=> x.wokrOrderId)
+            .NotEmpty()
+            .WithMessage("Work Order Id must be provided.")
+            .WithErrorCode("WorkOrderId_Required")
+            ;
+    }
+}

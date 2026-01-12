10ef45109fbeb536331a0683e257fa13ac716885
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2026-01-12 23:17:51 +0100

    refactor: Could should not contain blank lines at the end of the file, see SA1518

--- src/MechanicApplication/Features/WorkOrders/Commands/DeleteWorkOrder/DeleteWorkOrderCommand.cs
+++ src/MechanicApplication/Features/WorkOrders/Commands/DeleteWorkOrder/DeleteWorkOrderCommand.cs
@@ -6,4 +6,3 @@ namespace MechanicApplication.Features.WorkOrders.Commands.DeleteWorkOrder;
 
 public sealed record class DeleteWorkOrderCommand(Guid workOrderId) 
     : IRequest<ErrorOr<Deleted>>;
-
